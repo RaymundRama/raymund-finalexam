e589e3c0309b593e845f16f393997ca7c6dbcf06
Raymund <71455069+RaymundRama@users.noreply.github.com>
2023-12-13 19:40:26 -0500

    implementing endpoints to handle CRUD

---
+++ routes/api/restaurants.js
@@ -0,0 +1,17 @@
+// Router object for /api/restaurants endpoint
+// Import Express
+const express = require("express");
+// create router object
+const router = express.Router();
+// import model
+const Restaurant = require("../../models/restaurant")
+
+// Configure handlers
+// TODO add CRUD functionality by adding handlers for these HTTP Methods
+// Create mapped to POST
+// Read mapped to GET
+// Update mapped to PUT
+// Delete mapped to DELETE
+
+//Export 
+module.exports = router;
